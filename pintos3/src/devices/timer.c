b56b68e37bba719f7599b0b343099e5497c680eb
limiao <limiao@shanghaitech.edu.cn>
2020-12-25 10:21:10 +0800

    org

--- pintos3/src/devices/timer.c
+++ pintos3/src/devices/timer.c
@@ -60,7 +60,7 @@ timer_calibrate (void)
   /* Refine the next 8 bits of loops_per_tick. */
   high_bit = loops_per_tick;
   for (test_bit = high_bit >> 1; test_bit != high_bit >> 10; test_bit >>= 1)
-    if (!too_many_loops (high_bit | test_bit))
+    if (!too_many_loops (loops_per_tick | test_bit))
       loops_per_tick |= test_bit;
 
   printf ("%'"PRIu64" loops/s.\n", (uint64_t) loops_per_tick * TIMER_FREQ);
@@ -89,16 +89,11 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  if (ticks <= 0)
-    return;
+  int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  /* intr_level == INTR_ON, turn off */
-  enum intr_level old_level = intr_disable ();
-  thread_current()->ticks_sleep = ticks;
-  /* Block threads that needs to sleep*/
-  thread_block();
-  intr_set_level (old_level);
+  while (timer_elapsed (start) < ticks) 
+    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -176,10 +171,6 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
-	/* Every tick, check if any threads are sleeping*/
-  enum intr_level old_level = intr_disable ();
-  thread_foreach (check_sleep, NULL);
-  intr_set_level (old_level);
   thread_tick ();
 }
 
